16e72a84da3ca38119cc8fb3a5f4990ce513effc
shadow <97516081+yesudas-philiph@users.noreply.github.com>
2022-02-19 17:00:31 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,7 +1,3 @@
-##====================================
-###docker 
-##====================================
-
 #!/bin/bash
 
 echo "ClientAliveInterval 60" >> /etc/ssh/sshd_config
@@ -12,3 +8,5 @@ echo "LC_ALL=en_US.utf-8" >> /etc/environment
 amazon-linux-extras install docker -y
 systemctl start docker
 systemctl enable docker
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
+chmod +x /usr/bin/docker-compose
