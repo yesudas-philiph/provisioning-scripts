1ce44cb9b111f95f14dc868a799f7de6f2e9d88d
yesudas_philiph <yesudasphilip03@gmail.com>
2022-02-20 21:22:29 +0530

    message

---
+++ basic_userdata.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "ClientAliveInterval 60" >> /etc/ssh/sshd_config
+echo "LANG=en_US.utf-8" >> /etc/environment
+echo "LC_ALL=en_US.utf-8" >> /etc/environment
+
+#for keeping the instance history after reboot for root and ec2-user
+
+for i in "/root/.bashrc" "/home/ec2-user/.bashrc"
+do
+   cat <<EOF >> $i
+# Avoid duplicates
+HISTCONTROL=ignoredups:erasedups
+# When the shell exits, append to the history file instead of overwriting it
+shopt -s histappend
+
+# After each command, append to the history file and reread it
+PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}history -a; history -c; history -r"
+EOF
+done
+
